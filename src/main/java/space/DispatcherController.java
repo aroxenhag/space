7f71a6f850c7f2e1a5c67553a33deb686617f99c
Giulio Mola <gmola@atex.com>
2016-10-07 16:35:23 +0200

    use format for images

--- src/main/java/space/DispatcherController.java
+++ src/main/java/space/DispatcherController.java
@@ -319,7 +319,7 @@ public class DispatcherController {
             } else if (id.startsWith("contentid/")) {
                 id = id.substring("contentid/".length());
             }
-            return imageServiceBaseUrl + "/" + id + "/image.jpg?a=2:1";
+            return imageServiceBaseUrl + "/" + id + "/image.jpg?f=2x1";
         }
     }
 }
